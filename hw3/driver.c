390791a3430d06d935ec432b2a9029d1f97ae940
Yang <gangyang@umich.edu>
2019-12-25 03:10:22 +0000

    upload hw3

---
+++ hw3/driver.c
@@ -0,0 +1,166 @@
+/*
+ * 1. Set a breakpoint in new_Stack. What is the address returned by malloc()?
+ * -  (void *) 0x603010
+ * 2. What is the address returned by new_Stack?
+ * - (Stack *) 0x603028
+ * 3. Is this address on the stack, heap, text, or data?
+ * - stack
+ * 4. Now set a breakpoint in delete_Stack. What is the the value of spp? Is
+ * this value on the stack, heap, text, or data?
+ * - spp = (Stack **) 0x7fffffffd988
+ * - address on stack
+ * 5. What is the value of *spp? Is this an address on the stack, heap, text
+ * or data?
+ * - *spp = (Stack *) 0x603028
+ * - address on heap
+ * 6. What is the value of the parameter being passed to free()?
+ * - (void *) 0x603010
+ * 7. Is this parameter to free the same address that was returned from malloc
+ * (Your answer to question 1)? If it isn't, then the address you are passing
+ * is incorrect, and you should modify your code to fix this problem.
+ * - yes it is*/
+
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include "mylib.h"
+#include "stack.h"
+
+int main (int argc, char * const * argv) {
+
+        Stack * main_Stack = 0;         /* the test stack */
+        unsigned long amount;        /* numbers of items possible go on stack */
+        long command;                   /* stack command entered by user */
+        long item = 0;                  /* item to go on stack */
+        char option;                    /* the command line option */
+        long status;                    /* return status of stack functions */
+        
+        /* initialize debug states */
+        debug_off ();
+
+        /* check command line options for debug display */
+        while ((option = getopt (argc, argv, "x")) != EOF) {
+                
+                switch (option) {
+                        case 'x': debug_on (); break;
+                }
+        }
+
+        while (1) {
+                command = 0;            /* initialize command, need for loops */
+                writeline ("\nPlease enter a command:", stdout);
+                writeline ("\n\t(a)llocate, (d)eallocate, ", stdout);
+                writeline ("p(u)sh, (p)op, (t)op, (i)sempty, (e)mpty, ",stdout);
+                writeline ("\n\tis(f)ull, (n)um_elements, (w)rite to stdout, "
+                                                                , stdout);
+                writeline ("(W)rite to stderr.\n", stdout);
+                writeline ("Please enter choice:  ", stdout);
+                command = getchar ();
+                if (command == EOF)     /* are we done? */
+                        break;
+                clrbuf (command);       /* get rid of extra characters */
+
+                switch (command) {      /* process commands */
+
+                
+		case 'a':               /* creat a new stack*/
+		        writeline ("\nPlease enter the number of objects to"
+			"be able to store:",stdout);
+			amount = decin();
+			clrbuf(0);
+			/* check if main_Stack already has element in it,
+			 * if is, call delete_Stack*/
+			if (main_Stack != 0){
+                            delete_Stack(&main_Stack);
+			}
+			main_Stack = new_Stack(amount);
+			break;
+		case 'd':               /* delete current stack*/
+		        delete_Stack (&main_Stack);
+			writeline ("Stack had been deleted.\n",stdout);
+			break;
+		case 'e':               /* empty current stack*/
+		        empty_Stack(main_Stack);
+			writeline ("Stack is empty.\n",stdout);
+		        break;
+
+                case 'f':               /* isfull */
+                        if (isfull_Stack (main_Stack))
+                                writeline ("Stack is full.\n",stdout);
+                        else
+                                writeline ("Stack is not full.\n", stdout);
+                        break;
+
+                case 'i':               /* isempty */
+                        if (isempty_Stack (main_Stack))
+                                writeline ("Stack is empty.\n", stdout);
+                        else
+                                writeline ("Stack is not empty.\n", stdout);
+                        break;
+
+                case 'n':               /* num_elements */
+                        writeline ("Number of elements on the stack is:  ",
+                                                                    stdout);
+                        decout (num_elements (main_Stack));
+                        newline ();
+                        break;
+
+                case 'p':               /* pop */
+                        status = pop (main_Stack, &item);
+                        if (! status)
+                                fprintf (stderr,"\nWARNING:  pop FAILED\n");
+                        else {
+                                writeline (
+                                        "Number popped from the stack is:  ",
+                                                                    stdout);
+                                decout (item);
+                                newline ();
+                        }
+                        break;
+
+                case 't':               /* top */
+                        status = top (main_Stack, &item);
+                        if (! status)
+                                fprintf (stderr,"\nWARNING:  top FAILED\n");
+                        else {
+                                writeline (
+                                        "Number at top of the stack is:  ",
+                                                                    stdout);
+                                decout (item);
+                                newline ();
+                        }
+                        break;
+
+                case 'u':               /* push */
+                        writeline (
+                                "\nPlease enter a number to push to stack:  ",
+                                                                    stdout);
+                        item = decin ();
+                        clrbuf (0);     /* get rid of extra characters */
+                        status = push (main_Stack, item);
+                        if (! status)
+                                fprintf(stderr,"\nWARNING:  push FAILED\n");
+                        break;
+
+                case 'w':               /* write */
+                        writeline ("\nThe Stack contains:\n", stdout);
+                        write_Stack (main_Stack, stdout);
+                        break;
+
+                case 'W':               /* write */
+                        writeline ("\nThe Stack contains:\n", stdout);
+                        write_Stack (main_Stack, stderr);
+                        break;
+                }
+
+                if (item == EOF) /* check if done */
+                        break;
+        }
+
+        if (main_Stack)
+                delete_Stack (&main_Stack);     /* deallocate stack */
+        newline ();
+        return 0;
+}
+
